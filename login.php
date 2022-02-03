2bcdca641e27ac2d429a2e58a04bf2ff35564569
Guilherme <89256044+guilhermefrl@users.noreply.github.com>
2022-02-03 15:08:22 +0000

    Fix

--- login.php
+++ login.php
@@ -54,5 +54,36 @@
                 </div>
             </div>
         </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+        </svg>
+
+        <?php
+            if (isset($_GET["success"])){
+                if($_GET["success"] == "Account_Created"){
+                    echo '
+                        <div class="register_alert_style alert alert-success alert-dismissible" id="Alert" role="alert">
+                            <div class="d-flex align-items-center register_alert_text4">
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                                <div>
+                                    <strong>Success!</strong> Account created.
+                                </div>
+                            </div>
+                        </div>
+                    ';
+                }
+            }
+        ?>
     </body>
 </html>
+
+<script>
+    var myAlert = document.getElementById('Alert')
+    myAlert.addEventListener('closed.bs.alert', function () {
+        window.location.href = '/Password-Manager/login.php';
+    })
+</script>
